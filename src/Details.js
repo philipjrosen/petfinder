6cdb28e838ba9ccc6b7aa0170e14a975ca6014f2
Philip Rosen <philipjrosen@gmail.com>
2018-08-27 13:54:25 -0700

    implement Details component

--- src/Details.js
+++ src/Details.js
@@ -1,8 +1,64 @@
 import React from "react";
+import pf from "petfinder-client";
+import { navigate } from "@reach/router";
+
+// api client
+const petfinder = pf({
+  key: process.env.API_KEY,
+  secret: process.env.API_SECRET
+});
 
 export class Details extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true
+    };
+  }
+  componentDidMount() {
+    petfinder.pet
+      .get({
+        output: "full",
+        id: this.props.id
+      })
+      .then(data => {
+        const pet = data.petfinder.pet;
+        let breed;
+        if (Array.isArray(pet.breeds.breed)) {
+          breed = pet.breeds.breed.join(", ");
+        } else {
+          breed = pet.breeds.breed;
+        }
+        this.setState({
+          name: pet.name,
+          animal: pet.animal,
+          location: `${pet.contact.city}, ${pet.contact.state}`,
+          description: pet.description,
+          media: pet.media,
+          breed,
+          loading: false
+        });
+      })
+      .catch(() => {
+        navigate("/");
+      });
+  }
   render() {
-    return <div>Hey Hey</div>;
+    if (this.state.loading) {
+      return <h1>loading … </h1>;
+    }
+
+    const { animal, breed, location, description } = this.state;
+
+    return (
+      <div className="details">
+        <div>
+          <h1>{name}</h1>
+          <h2>{`${animal} — ${breed} — ${location}`}</h2>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
   }
 }
 
