b0b0f5b420fc98e956a06782545174f2bc6ba8e9
Philip Rosen <philipjrosen@gmail.com>
2018-08-27 13:07:53 -0700

    add more properties to Pet

--- src/App.js
+++ src/App.js
@@ -54,6 +54,8 @@ class App extends React.Component {
               key={pet.id}
               name={pet.name}
               breed={breed}
+              media={pet.media}
+              location={`${pet.contact.city}, ${pet.contact.state}`}
             />
           );
         })}
