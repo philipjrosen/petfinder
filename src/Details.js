6622bd528dba3979c421ba2ccaf98b436e2ac8c6
Philip Rosen <philipjrosen@gmail.com>
2018-08-28 08:17:54 -0700

    add Carousel

--- src/Details.js
+++ src/Details.js
@@ -1,6 +1,7 @@
 import React from "react";
 import pf from "petfinder-client";
 import { navigate } from "@reach/router";
+import Carousel from "./Carousel";
 
 // api client
 const petfinder = pf({
@@ -45,10 +46,11 @@ export class Details extends React.Component {
       return <h1>loading … </h1>;
     }
 
-    const { animal, breed, location, description } = this.state;
+    const { animal, breed, location, description, media } = this.state;
 
     return (
       <div className="details">
+        <Carousel media={media} />
         <div>
           <h1>{name}</h1>
           <h2>{`${animal} — ${breed} — ${location}`}</h2>
